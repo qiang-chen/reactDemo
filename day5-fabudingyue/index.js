928388b6e5b09f8db86be7a70fac9e4ab6ca2a72
qiang <1715584439@qq.com>
2019-06-14 10:17:22 +0800

    发布订阅者的封装

---
+++ day5-fabudingyue/index.js
@@ -0,0 +1,84 @@
+
+//第一种方式利用数组是比较啰嗦的
+// const opj={
+//     arr:[],
+//     $on(event,cb){
+//         this.arr.push({[event]:cb})
+//     },
+//     $emit(event,msg){
+//         //console.log(this.arr)
+//         this.arr.forEach(item=>{
+//             //console.log(Object.keys(item))
+//             if(Object.keys(item)[0]===event){
+//                 item[Object.keys(item)[0]](msg)
+//             }
+//         })
+//     }
+// }
+
+
+//第二种方式利用对象
+
+//这种方式只能监听一次  假如我们想要监听多次可以看下下面第三种方法
+// const opj={
+//     arr:{},
+//     $on(event,cb){
+//         this.arr[event]=cb
+//     },
+//     $emit(event,msg){
+//         this.arr[event](msg)
+//     }
+// }
+
+
+//第三种方式 多次监听的订阅者
+const opj={
+    arr:{},
+    //订阅者监听函数
+    $on(event,cb){
+        if(this.arr[event]){
+            this.arr[event].push(cb)
+        }else{
+            this.arr[event]=[cb]
+        }
+        
+    },
+    //发布者触发函数
+    $emit(event,msg){
+        this.arr[event]&&this.arr[event].forEach(cb=>{
+            cb(msg)
+        })
+    },
+    //销毁阶段
+    $delete(event){
+        delete this.arr[event]
+    }
+}
+
+
+
+opj.$on("up",(msg)=>{
+    //订阅者监听函数
+    console.log(msg)
+})
+opj.$on("up",(msg)=>{
+    //订阅者监听函数
+    console.log(msg)
+})
+
+opj.$delete("up")
+
+opj.$on("down",(msg)=>{
+    //订阅者监听函数
+    console.log(msg)
+})
+
+
+
+opj.$emit("up",123)//发布者 做函数的触发
+
+
+opj.$emit("down",456)//发布者
+
+
+//这样功能就可以实现了然后抛出使用了
