5e72c83736d94829731ecfd46a30e064e3198af1
qiang <1715584439@qq.com>
2019-06-15 07:56:57 +0800

    发布订阅的对象完美版封装

--- day5-fabudingyue/index.js
+++ day5-fabudingyue/index.js
@@ -1,6 +1,6 @@
 
 //第一种方式利用数组是比较啰嗦的
-// const opj={
+// const Observer={
 //     arr:[],
 //     $on(event,cb){
 //         this.arr.push({[event]:cb})
@@ -20,7 +20,7 @@
 //第二种方式利用对象
 
 //这种方式只能监听一次  假如我们想要监听多次可以看下下面第三种方法
-// const opj={
+// const Observer={
 //     arr:{},
 //     $on(event,cb){
 //         this.arr[event]=cb
@@ -32,7 +32,7 @@
 
 
 //第三种方式 多次监听的订阅者
-const opj={
+const Observer={
     arr:{},
     //订阅者监听函数
     $on(event,cb){
@@ -57,28 +57,31 @@ const opj={
 
 
 
-opj.$on("up",(msg)=>{
+//以下为封装检验过程
+
+
+Observer.$on("up",(msg)=>{
     //订阅者监听函数
     console.log(msg)
 })
-opj.$on("up",(msg)=>{
-    //订阅者监听函数
-    console.log(msg)
-})
-
-opj.$delete("up")
-
-opj.$on("down",(msg)=>{
+Observer.$on("up",(msg)=>{
     //订阅者监听函数
     console.log(msg)
 })
 
+Observer.$delete("up")
+
+Observer.$on("down",(msg)=>{
+    //订阅者监听函数
+    console.log(msg)
+})
 
 
-opj.$emit("up",123)//发布者 做函数的触发
+
+Observer.$emit("up",123)//发布者 做函数的触发
 
 
-opj.$emit("down",456)//发布者
+Observer.$emit("down",456)//发布者
 
 
 //这样功能就可以实现了然后抛出使用了
